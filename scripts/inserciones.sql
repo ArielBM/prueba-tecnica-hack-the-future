b80303ae29a486957affbc52c8d07ed95293851f
ArielBM <201503910@ingenieria.usac.edu.gt>
2023-04-01 23:35:34 -0600

    Commit inicial
    ...

---
+++ scripts/inserciones.sql
@@ -0,0 +1,64 @@
+insert into recursos(rec_descripcion)
+values ('combustible'),
+		('energía electrica'),
+		('ceite'),
+		('efrigerante');
+		
+insert into recursos(rec_descripcion)
+values ('hojas');
+		
+
+insert into tipo_emisiones(tem_descripcion)
+values ('emision directa'),
+		('emision indirecta'),
+		('emisiones indirectas');
+		
+insert into unidades(uni_abrevitatura,uni_descripcion)
+values ('gl','galones'),
+		('kW', 'Kilo Watts'),
+		('ud','unidades');
+		
+		
+insert into tipo_consumo(tco_descripcion)
+values 	('administrativo'),
+		('indirecto de proveedores'),
+		('logístico'),
+		('distribución'),
+		('operación');
+		
+		
+insert into meses(mes_descripcion)
+values 	('enero'),
+		('febrero'),
+		('marzo'),
+		('abril'),
+		('mayo'),
+		('junio'),
+		('julio'),
+		('agosto'),
+		('septiembre'),
+		('octubre'),
+		('noviembre'),
+		('diciembre');
+		
+		
+insert into anios(ani_descripcion)
+values 	('2022'),
+		('2023'),
+		('2024'),
+		('2025'),
+		('2026'),
+		('2027'),
+		('2028'),
+		('2029'),
+		('2030'),
+		('2031'),
+		('2032'),
+		('2033'),
+		('2034');
+		
+		
+insert into equipos(equ_descripcion)
+values 	('ventas'),
+		('administrativo');
+		
